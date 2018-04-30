5aea250b309e7015ca4506976d2d3528997f0d14
Bobby Caya <bob.caya1@gmail.com>
2018-04-30 10:24:32 -0600

    Rebuilding test helper add renderComponent and export

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,11 +1,20 @@
 import jsdom from 'jsdom';
-import _$ from 'jquery'; 
+import _$ from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+import { expect } from 'chai';
 // Set up testing environment to run like a browser in the command line 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView; 
 const $ = _$(global.window);
 //build 'renderComponent' helper that should render a given react clsas
+function renderComponent(ComponentClass){ 
+  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
 
+  return $ (ReactDOM.findDOMNode(componentInstance));
+}
 // build helper for simulating events 
 
 // set up chai-jquery
+
+export { renderComponent, expect }
