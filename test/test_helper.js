e19c6a87ca5a1b8ccc62c0161cccf6d9cd9cd1c8
Bobby Caya <bob.caya1@gmail.com>
2018-04-30 10:37:29 -0600

    finishing up renderComponent testhelper

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,14 +3,22 @@ import _$ from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
 import { expect } from 'chai';
+import React from 'react';
+import { Provider } from 'react-redux'; 
+import { createStore } from 'redux'; 
+import reducers from '../src/reducers'; 
+
 // Set up testing environment to run like a browser in the command line 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView; 
 const $ = _$(global.window);
 //build 'renderComponent' helper that should render a given react clsas
-function renderComponent(ComponentClass){ 
-  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
-
+function renderComponent(ComponentClass, props, state){ 
+  const componentInstance = TestUtils.renderIntoDocument(
+    <Provider store={createStore(reducers, state)}>
+      <ComponentClass {...props}/>
+    </Provider> 
+  );
   return $ (ReactDOM.findDOMNode(componentInstance));
 }
 // build helper for simulating events 
